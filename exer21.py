f83ca23db19a985be548e7a53a777cbd5e4fdc01
calouros <calouros@fatec.corp>
2024-09-25 10:25:15 -0300

    dois exer

---
+++ exer21.py
@@ -0,0 +1,10 @@
+print("Clara Vecchio Machado da Silva\nRA: 1051392421023 \nDSM - 1°Semestre\n")
+
+var1 = int(input("Digite o primeiro valor: "))
+var2 = int(input("Digite o segundo valor: "))
+var3 = int(input("Digite o terceiro valor: "))
+
+valor = var1*var2*var3
+media = valor/3
+
+print("A média aritmética é", media)
